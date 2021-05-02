467fdd1741deae0d0f40ce1b30c1a0eb40df0708
krishpranav <krisna.pranav@gmail.com>
2021-05-02 18:32:20 +0530

    global update

--- usrosint/core/run.py
+++ usrosint/core/run.py
@@ -20,7 +20,7 @@ def run(self):
 
         modules = self.get_report_modules()
 
-        print("\n" + "Profil3r will search : \n " + Colors.BOLD + "[+] " + Colors.ENDC +  "{} \n".format(str('\n ' + Colors.BOLD + "[+] " + Colors.ENDC).join(modules)))
+        print("\n" + "usrosint will search : \n " + Colors.BOLD + "[+] " + Colors.ENDC +  "{} \n".format(str('\n ' + Colors.BOLD + "[+] " + Colors.ENDC).join(modules)))
 
         for module in modules:
             thread = threading.Thread(target=self.modules[module]["method"])
