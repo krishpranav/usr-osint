ac736f81b25e04e1b66250a2651553cfed3544c4
krishpranav <krisna.pranav@gmail.com>
2021-05-03 11:59:16 +0530

    global bug update

--- usrosint/core/run.py
+++ usrosint/core/run.py
@@ -1,29 +1,23 @@
+#!/usr/bin/env python3
+
 from usrosint.core.colors import Colors
 import threading
 
 def run(self):
     self.print_logo()
+    self.parse_arguments()
 
-    # No argument
-    if not len(self.items):
-        print('''usr-osint is an OSINT tool that allows you to find potential profiles of a person on social networks, as well as their email addresses and shows you the data leak of the found emails.
-    Usage : ./usrosint.py <arguments>
-    for exemple : ./usrosint.py user name
-                ./usrosint.py user name''')
+    self.menu()
+    self.get_permutations()
 
-    else:
-        self.menu()
-        self.get_permutations()
+    print(Colors.BOLD + "[+]" + Colors.ENDC + " {} permutations to test for each service, you can reduce this number by selecting less options if it takes too long".format(len(self.permutations_list)))
 
-        # Number of permutations to test per service
-        print(Colors.BOLD + "[+]" + Colors.ENDC + " {} permutations to test for each service, you can reduce this number by selecting less options if it takes too long".format(len(self.permutations_list)))
+    modules = self.get_report_modules()
 
-        modules = self.get_report_modules()
+    print("\n" + "usr-osint will search : \n " + Colors.BOLD + "[+] " + Colors.ENDC +  "{} \n".format(str('\n ' + Colors.BOLD + "[+] " + Colors.ENDC).join(modules)))
 
-        print("\n" + "usrosint will search : \n " + Colors.BOLD + "[+] " + Colors.ENDC +  "{} \n".format(str('\n ' + Colors.BOLD + "[+] " + Colors.ENDC).join(modules)))
-
-        for module in modules:
-            thread = threading.Thread(target=self.modules[module]["method"])
-            thread.start()
-            thread.join()
-        self.generate_report()
+    for module in modules:
+        thread = threading.Thread(target=self.modules[module]["method"])
+        thread.start()
+        thread.join()
+    self.generate_report()
